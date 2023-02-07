2ab564e19e1fe185d6cad3732a03d46b58a55558
Progga <proggaroy2000@gmail.com>
2023-02-07 18:19:58 +0600

    array slice and splice

---
+++ README.md
@@ -0,0 +1,1 @@
+# Array-Object-Some-Prac-Problem
---
+++ slice.js
@@ -0,0 +1,10 @@
+// array slice
+const friends = [34,12,45,67,82,11,22,9,78,90,120,5];
+const partial = friends.slice(4,8);
+// console.log(partial);
+
+// array splice
+const friends1 = [34,12,45,67,82,11,22,9,78,90,120,5];
+const partial1 = friends1.splice(3,7);
+console.log(partial1);
+console.log(friends1);
